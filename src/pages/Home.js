f25f3067543676f606be3236beeaa4c88ed6589f
Joel-100 <joelkugonza@gmail.com>
2022-11-24 20:21:23 +0300

    Dispatch action to get data in component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { fetchCountry } from '../store/action';
 import africaImg from '../assets/africa.png';
 
-const Home = () => (
-  <div>
-    <div className="flex p-5 items-center border-b">
-      <img src={africaImg} alt="Africa map" className="w-[60%] opacity-60" />
-      <div className="ml-6">
-        <h3 className="text-3xl font-mono text-white font-bold">Africa</h3>
-        <p className="text-xl text-white">1,393.7 billion</p>
+const Home = () => {
+  let retrieveOnce = true;
+  const country = useSelector((state) => state.country, shallowEqual);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (retrieveOnce) {
+      dispatch(fetchCountry());
+      retrieveOnce = false;
+    }
+  }, [dispatch]);
+
+  console.log(country);
+
+  return (
+    <div>
+      <div className="flex p-5 items-center border-b">
+        <img src={africaImg} alt="Africa map" className="w-[60%] opacity-60" />
+        <div className="ml-6">
+          <h3 className="text-3xl font-mono text-white font-bold">Africa</h3>
+          <p className="text-xl text-white">1,393.7 billion</p>
+        </div>
+      </div>
+      <div className="text-white my-4">
+        <h3 className="ml-8">Stats by Country</h3>
       </div>
     </div>
-    <div className="text-white my-4">
-      <h3 className="ml-8">Stats by Country</h3>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
