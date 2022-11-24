6bdbe9547797b100eeca90163a0892498861cb00
Joel-100 <joelkugonza@gmail.com>
2022-11-24 21:46:06 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Details from './components/Details';
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <NavBar>Component</NavBar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:id" element={<Details />} />
