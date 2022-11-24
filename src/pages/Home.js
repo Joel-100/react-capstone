741eef18a33cf0fee70448f424d7361e8eb6b625
Joel-100 <joelkugonza@gmail.com>
2022-11-24 20:10:30 +0300

    Create layout of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import africaImg from '../assets/africa.png';
 
 const Home = () => (
-  <div>Home</div>
+  <div>
+    <div className="flex p-5 items-center border-b">
+      <img src={africaImg} alt="Africa map" className="w-[60%] opacity-60" />
+      <div className="ml-6">
+        <h3 className="text-3xl font-mono text-white font-bold">Africa</h3>
+        <p className="text-xl text-white">1,393.7 billion</p>
+      </div>
+    </div>
+    <div className="text-white my-4">
+      <h3 className="ml-8">Stats by Country</h3>
+    </div>
+  </div>
 );
 
 export default Home;
